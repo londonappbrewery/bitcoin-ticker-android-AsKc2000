7840d28c9583c6859a219bccd336221ce648ab1c
AsKc2000 <chaitanyakrishnasai123@gmail.com>
2019-06-14 17:42:12 +0530

    project done and app running succesfully

--- app/src/main/java/com/londonappbrewery/bitcointicker/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/bitcointicker/MainActivity.java
@@ -12,19 +12,28 @@ import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.Toast;
 
 
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.net.URISyntaxException;
+
+import cz.msebera.android.httpclient.Header;
 
 
 public class MainActivity extends AppCompatActivity {
 
     // Constants:
     // TODO: Create the base URL
-    private final String BASE_URL = "https://apiv2.bitcoin ...";
+    private final String BASE_URL = "https://apiv2.bitcoinaverage....";
 
     // Member Variables:
     TextView mPriceTextView;
+    String LOGCAT_TAG = "BITCOIN";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +54,21 @@ public class MainActivity extends AppCompatActivity {
         spinner.setAdapter(adapter);
 
         // TODO: Set an OnItemSelected listener on the spinner
+        spinner.setOnItemSelectedListener(new OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String currency = (String) parent.getItemAtPosition(position);
+                Log.d(LOGCAT_TAG, "onItemSelected: " + currency);
+                String RequestURL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTC" + currency;
+                letsDoSomeNetworking(RequestURL);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                Log.d(LOGCAT_TAG, "NothingSelected ");
+
+            }
+        });
 
     }
 
@@ -71,8 +95,34 @@ public class MainActivity extends AppCompatActivity {
 //                Toast.makeText(WeatherController.this, "Request Failed", Toast.LENGTH_SHORT).show();
 //            }
 //        });
+        AsyncHttpClient client=new AsyncHttpClient();
+        client.get(url,new JsonHttpResponseHandler(){
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                Log.d(LOGCAT_TAG, "onSuccess: "+response.toString());
+                super.onSuccess(statusCode, headers, response);
 
+                try {
+                    String  price=response.getString("ask");;
 
+                    updateUI(price);
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                super.onFailure(statusCode, headers, throwable, errorResponse);
+
+            }
+        });
+
+    }
+    public void updateUI(String price){
+        mPriceTextView.setText(price);
     }
 
 
